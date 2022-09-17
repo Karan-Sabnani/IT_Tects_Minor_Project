0668a17e1d77a710fd99baa68969a8ca960cb1a7
Karan Sabnani <86863201+Karan-Sabnani@users.noreply.github.com>
2022-09-17 09:08:06 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Crypto Coven Dashboard",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
@@ -16,7 +16,7 @@ export const CONFIG = {
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://i.seadn.io/gae/VzyFhTxAQWIjNdDKBCpgl-sVomab0mmFQpVgk6_2YY4LqFaitf3RsvHiFJ5JmDxck6mxTHQkcVsdY-oxrUjdoEfhkRjHormYrvLLc6U?w=500&auto=format",
   },
 
   // Supported networks - just for your reference
